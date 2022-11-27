bb2929dd693225f8311f4f4f51bc7672d40f1dde
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2022-11-27 12:55:22 +0000

    Remove namespace on database migrations

--- database/migrations/2019_06_30_000300_create_smsnotify_tables.php
+++ database/migrations/2019_06_30_000300_create_smsnotify_tables.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace IgniterLabs\SmsNotify\Database\Migrations;
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
